59213418d25c32732ec8ad1596c146e4033a67a6
fabio758 <fabioluisdossantos998@gmail.com>
2025-03-27 11:08:28 -0300

    criacao da tela cadastro

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/criar-conta', function () {
+    return view('criar-conta');
+});
 
 Route::get('/teste', function () {
     return "o código foi testado";
